689c2215ca2afa5cc094b45f42e8340a5e2af4ed
The3Legend <durdvlad@gmail.com>
2021-11-11 14:13:14 +0300

    fix

--- js/progresBar.js
+++ js/progresBar.js
@@ -2,12 +2,11 @@ function userProgress() {
     let start = 0;
     let progressElement = document.querySelectorAll('.user-progress')
     progressElement.forEach(el => {
-        console.log(el.value)
 
         setInterval(() => {
             el.value = start;
             start++
-        }, 300)
+        }, 350)
     })
 }
 userProgress()
